a41cc262fb49d29d3b0ab817d2175343ce56c4a2
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-04-08 21:53:53 +0100

    ch: installed apex chart

--- src/modules/page/Revenue/components/chart/index.tsx
+++ src/modules/page/Revenue/components/chart/index.tsx
@@ -1,11 +1,24 @@
-
+import { useGetWallet } from "@hooks/revenue/useGetWallet";
 
 const Chart = () => {
+    const {walletData} = useGetWallet()
   return (
-    <div className=" col-span-2 bg-green-300">
-        <div></div>
-    </div>
-  )
-}
+    <div className=" col-span-2">
+      <div className="flex gap-[64px] items-center">
+        <div>
+          <p>Available Balance</p>
+          <p className="text-[36px] leading-[48px] font-bold text-black">
+            USD <span>{walletData?.balance.toFixed(2)}</span>
+          </p>
+        </div>
 
-export default Chart
+       
+        <button className="text-[16px] leading-[24px] font-[600] text-[#fff] bg-black  py-[14px] rounded-[100px] w-[167px]">
+          Withdraw
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Chart;
